4c9e0e85dbe34305a19317c4c148d53079e4a2d5
TValeriiaM <149121749+TValeriiaM@users.noreply.github.com>
2024-07-23 14:35:23 +0300

    fin

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,28 @@
+// import { HttpError } from 'http-errors';
+import { isHttpError } from 'http-errors';
+
+export const errorHandler = (err, _req, res, _next) => {
+  /// Ось цей код з конспекту не спрацював, видавало помилку 500 на неіснуючий ID контакту..///
+
+  //   if (err instanceof HttpError) {
+  //     res.status(err.status).json({
+  //       status: err.status,
+  //       message: err.name,
+  //       data: err,
+  //     });
+  //     return;
+  //   }
+
+  if (isHttpError(err) === true) {
+    return res.status(err.status).send({
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
+};
