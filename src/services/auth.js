af6f42362224aaa86a1f0e55c1174ad462bd15ae
TValeriiaM <149121749+TValeriiaM@users.noreply.github.com>
2024-08-11 13:09:58 +0300

    begin

--- src/services/auth.js
+++ src/services/auth.js
@@ -77,3 +77,12 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
     ...newSession,
   });
 };
+
+export const requestResetToken = async (email) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  //доповнимо її трохи пізніше
+};
