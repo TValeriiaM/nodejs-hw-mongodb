6afd47739fcfb47fedb89780cf0facb9f32fd619
TValeriiaM <149121749+TValeriiaM@users.noreply.github.com>
2024-08-21 02:39:56 +0300

    swagger_contacts

--- src/server.js
+++ src/server.js
@@ -26,13 +26,13 @@ export const setupServer = () => {
   );
   app.use('/uploads', express.static(UPLOAD_DIR));
 
+  app.use('/api-docs', swaggerDocs());
+
   app.use(router);
 
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
-
-  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
